b6c01eb0b3e3d308a4b7986169a81c7cd2277ae1
Mengqi Zhu <treeforever7@gmail.com>
2019-10-20 21:40:47 -0400

    tooling

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = typeof NODE_ENV !== 'undefined' && NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
+const devtool = isProduction ? false : 'inline-source-map';
+
+module.exports = {
+  entry: {
+    app: ['./src/react-app/index.tsx', 'webpack-hot-middleware/client'],
+    vendor: ['react', 'react-dom']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js'
+  },
+  mode,
+  devtool,
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          compilerOptions: {
+            "sourceMap": !isProduction,
+          }
+        }
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'react-app', 'index.html') }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
